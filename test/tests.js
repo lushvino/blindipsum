43f2619472d224f3c527ae449da2dc23e1e0f4c2
Mark Lussier <mlussier@gmail.com>
2013-07-12 11:00:53 -0700

    initial test

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,14 @@
-var generator = require('../');
 var test = require('tap').test;
 
+var blindipsum = require('./../');
+var ipsum = blindipsum({});
+var dict = require('./../data/dictionary');
+
+
+test('html output', function (t) {
+   t.plan(1);
+   ipsum.setDictionary(dict);
+   var html = ipsum.generate({count: 5, unit: 'sentance', format: 'html'})
+   t.type(html, 'string')
+   t.end();
+});
